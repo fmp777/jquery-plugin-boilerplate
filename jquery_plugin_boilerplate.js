fd1e0b1cc28f46f7a094a18f8326a37773241945
fmp777 <noadvertise@gmail.com>
2017-08-20 13:11:37 -0600

    updated with actually useful comments

--- jquery_plugin_boilerplate.js
+++ jquery_plugin_boilerplate.js
@@ -11,47 +11,37 @@
 
     "use strict";
 
-    // undefined is used here as the undefined global variable in ECMAScript 3 is
-    // mutable (ie. it can be changed by someone else). undefined isn't really being
-    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-    // can no longer be modified.
-
-    // window and document are passed through as local variable rather than global
-    // as this (slightly) quickens the resolution process and can be more efficiently
-    // minified (especially when both are regularly referenced in your plugin).
-
     // Create the defaults once
-    var pluginName = "defaultPluginName",
+    var pluginName = "defaultPluginName",   // set your plugin name here ie: $(el).yourPlugin();
         defaults = {
-            propertyName: "value"
+            propertyName: "value"           // add all your default options/settings
         };
 
-    // The actual plugin constructor
+    // The actual plugin constructor - typically don't need to touch this
     function Plugin ( element, options ) {
         this.element = element;
 
-        // jQuery has an extend method which merges the contents of two or
-        // more objects, storing the result in the first object. The first object
-        // is generally empty as we don't want to alter the default options for
-        // future instances of the plugin
         this.settings = $.extend( true, {}, defaults, options );
         this._defaults = defaults;
         this._name = pluginName;
         this.init();
     }
 
-    // Avoid Plugin.prototype conflicts
+    // where your plugin lives! modify init and add all other functions you need!
     $.extend( Plugin.prototype, {
+        
         init: function() {
 
             // Place initialization logic here
-            // You already have access to the DOM element and
-            // the options via the instance, e.g. this.element
-            // and this.settings
+            // Access element via this.element
+            // Access options/settings via this.settings
+            
+            
             // you can add more functions like the one below and
             // call them like the example bellow
             this.yourOtherFunction( "jQuery Plugin Boilerplate" );
         },
+        
         yourOtherFunction: function( text ) {
 
             // some logic
@@ -64,10 +54,11 @@
     // You can instantiate your plugin first passing options, then subsequent calls will allow calling your functions
     // OR you can instantiate and call function at the same time if you have no options to pass
     // EXAMPLES:
-    //      $(element).defaultPluginName({option: value}) // instantiate
+    //      $(element).defaultPluginName({optin: value}) // instantiate
     //      $(element).defaultPluginName('method', [arg1, arg2, arg3]) will call yourOtherFunction(arg1, arg2, arg3)
     //      $(element).defaultPluginName('method', singleArgument) will call yourOtherFunction(singleArgument)
     $.fn[ pluginName ] = function( options, args ) {
+        
         return this.each( function() {
             var $plugin = $.data( this, "plugin_" + pluginName );
 
